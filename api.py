d7c7c11e006e21666c1954ee5f5efd78636a1142
Alina Biju <931601@swansea.ac.uk>
2024-11-19 22:48:59 +0000

    Create a list endpoint for the api.

--- api.py
+++ api.py
@@ -9,5 +9,12 @@ def homepage():
     return '<h1>Welcome to Api Home</h1>'
 
 
+@app.get('/objects')
+def retrievedata():
+    db_connector = DatabaseConnector()
+    data = db_connector.getAllObjects()
+
+    return data
+
 if __name__ == '__main__':
     app.run(port=5000,debug= True)
--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-@app.route('/mydataportal')
-def retrievedata():
-    return '<h1>API is working!!!</h1>'
-
-if __name__ == '__main__':
-    app.run(port=5000,debug= True)
