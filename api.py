a9c0eb44a0588604f2a7b23f9e5c3af456b265c6
Alina Biju <931601@swansea.ac.uk>
2024-11-22 00:18:19 +0000

    Enable cross origin for api.

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 from databaseConnector import DatabaseConnector
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
 def homepage():
