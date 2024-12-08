32c97090b32550f06afc7b8dcf9da3f808fc5fb3
Alina Biju <931601@swansea.ac.uk>
2024-12-08 23:18:39 +0000

    Update simulator to handle keyboard interrupt.

--- simulator.py
+++ simulator.py
@@ -2,11 +2,13 @@ import time
 import random
 from databaseConnector import DatabaseConnector
 
-def catalogueSimulator(conn):
+def catalogueSimulator(conn: DatabaseConnector):
+     
      all_items = conn.getAllObjects()
-     index = random.randrange(0,len(all_items))
 
+     index = random.randrange(0,len(all_items))
      record_to_update = all_items[index]
+
      if record_to_update['status']=='Available':
         record_to_update['status'] = 'Reserved'
      elif record_to_update['status'] == 'Reserved':
@@ -17,11 +19,16 @@ def catalogueSimulator(conn):
         print('Invalid State')
     
      conn.updateObjectState(record_to_update['id'],record_to_update['status'])
-     print(record_to_update)
-
+     print('Updated')
 
 if __name__ =="__main__":
-    dbase_conn = DatabaseConnector()
-    while True:
-        catalogueSimulator(dbase_conn)
-        time.sleep(60)
+    conn = DatabaseConnector()
+    try:
+        while True:
+                catalogueSimulator(conn)
+                time.sleep(60)
+    except KeyboardInterrupt:
+        print('Program Interrupted')
+        print('Exiting Code...')
+        conn.closeConnection()
+
